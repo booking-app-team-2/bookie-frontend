ffd5809ffbd5fa39475a91365f5e2ac2ad2ea8d3
darkokos <darkosvilar02@gmail.com>
2023-11-28 22:30:26 +0100

    fix: rebase merge conflict

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import {MaterialModule} from "../infrastructure/material/material.module";
-
+import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 
 @NgModule({
@@ -11,7 +12,9 @@ import {MaterialModule} from "../infrastructure/material/material.module";
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    MatButtonModule,
+    RouterLink,
   ],
   exports: [
     NavbarComponent
