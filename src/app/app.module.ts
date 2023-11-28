5fa2d841469497d6ae74703ca668fd837ce3dd5d
darkokos <darkosvilar02@gmail.com>
2023-11-28 22:29:32 +0100

    fix: rebase merge conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from "@angular/material/button";
+import {LayoutModule} from "./layout/layout.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    LayoutModule,
   ],
   providers: [
     provideClientHydration()
