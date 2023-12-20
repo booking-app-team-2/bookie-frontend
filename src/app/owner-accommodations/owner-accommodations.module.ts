4509d1ffcada1a8760cfd022c3919b02e4885e77
GojkoVuckovic <vuckovic.gojko1@gmail.com>
2023-12-20 16:28:53 +0100

    fix:changed directory for map and imported material module for owner accommodations

--- src/app/owner-accommodations/owner-accommodations.module.ts
+++ src/app/owner-accommodations/owner-accommodations.module.ts
@@ -3,13 +3,7 @@ import { CommonModule } from '@angular/common';
 import { OwnerAccommodationsComponent } from './owner-accommodations/owner-accommodations.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {LayoutModule} from "../layout/layout.module";
-import {MatButtonModule} from "@angular/material/button";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatDividerModule} from "@angular/material/divider";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatIconModule} from "@angular/material/icon";
-import {MatInputModule} from "@angular/material/input";
-
+import {MaterialModule} from "../infrastructure/material/material.module";
 
 
 @NgModule({
@@ -20,12 +14,7 @@ import {MatInputModule} from "@angular/material/input";
     CommonModule,
     FormsModule,
     LayoutModule,
-    MatButtonModule,
-    MatDatepickerModule,
-    MatDividerModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
+    MaterialModule,
     ReactiveFormsModule
   ]
 })
